b3584494538b70e87585ab91f87e0f3c1cf024a1
Patrick Duarte <36824550+SuegroLM@users.noreply.github.com>
2022-08-02 16:13:06 -0600

    Work with Strings - String Interpolation on Kotlin and XML
    Part 2 chapter 16
    
    Modified the result_dialog_message's text.
    Added a changeListener on the seekBar XMLobject to track its current value.
    
    Awaiting more changes.

--- app/src/main/java/com/sirstudios/bullseye/MainActivity.kt
+++ app/src/main/java/com/sirstudios/bullseye/MainActivity.kt
@@ -3,10 +3,13 @@ package com.sirstudios.bullseye
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.SeekBar
 import androidx.appcompat.app.AlertDialog
 import com.sirstudios.bullseye.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var sliderValue: Int = 0
+
     private lateinit var mainBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,13 +20,29 @@ class MainActivity : AppCompatActivity() {
             Log.i("Button click event", "You clicked the \"Hit Me!\" button")
             displayResult()
         }
+
+        mainBinding.seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                sliderValue = progress
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                TODO("Not yet implemented")
+            }
+
+        })
     }
 
     private fun displayResult() {
         val dialogTitle: String
             = getString(R.string.result_dialog_title)
         val dialogMessage: String
-            = getString(R.string.result_dialog_message)
+            //  = getString(R.string.result_dialog_message)
+            = getString(R.string.result_dialog_message, sliderValue)
         val builder: AlertDialog.Builder
             =   AlertDialog.Builder(this)
 
