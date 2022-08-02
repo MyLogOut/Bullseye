2535e14c288591f3e436b34c9d9c6c2945df2a00
Patrick Duarte <36824550+SuegroLM@users.noreply.github.com>
2022-08-02 12:19:43 -0600

    Initial commit

---
+++ app/src/main/java/com/sirstudios/bullseye/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.sirstudios.bullseye
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sirstudios.bullseye.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var mainBinding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
+    }
+}
