a9c476505575819e645075495c2b911c12695ac7
Patrick Duarte <36824550+SuegroLM@users.noreply.github.com>
2022-08-02 15:46:55 -0600

    Handle Interactions - Adding Alert Dialog
    Chapter 14, Part 2
    Added Alert Dialog when clicking on the "hit me!" button.
    Created the displayDialog() function to perform this action.

--- app/src/main/java/com/sirstudios/bullseye/MainActivity.kt
+++ app/src/main/java/com/sirstudios/bullseye/MainActivity.kt
@@ -2,6 +2,8 @@ package com.sirstudios.bullseye
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AlertDialog
 import com.sirstudios.bullseye.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,11 +13,27 @@ class MainActivity : AppCompatActivity() {
         mainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mainBinding.root)
 
-        with(mainBinding.hitMeButton) {
-            this.setOnClickListener {
-                this.width = (this.width * 1.2).toInt()
-                this.height = (this.height * 1.2).toInt()
-            }
+        mainBinding.hitMeButton.setOnClickListener {
+            Log.i("Button click event", "You clicked the \"Hit Me!\" button")
+            displayResult()
         }
+    }
+
+    private fun displayResult() {
+        val dialogTitle: String
+            = getString(R.string.result_dialog_title)
+        val dialogMessage: String
+            = getString(R.string.result_dialog_message)
+        val builder: AlertDialog.Builder
+            =   AlertDialog.Builder(this)
+
+        builder
+            .setTitle(dialogTitle)
+            .setMessage(dialogMessage)
+            .setPositiveButton(R.string.result_dialog_button) { dialog, _ ->
+                dialog.dismiss()
+            }
+            .create()
+            .show()
     }
 }
